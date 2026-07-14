bf5228b1d1073999ce3755cb3015d26dc46561fb
Vasyl Pryimak <vasyl.pryimak.dev@gmail.com>
2026-07-15 00:47:04 +0300

    feat(graphql): define initial blog schema and Prisma model relations

--- src/schema.ts
+++ src/schema.ts
@@ -4,4 +4,45 @@ export const typeDefs = gql`
   type Query {
     hello: String!
   }
+
+  type Mutation {
+    postCreate(post: PostInput!): PostPayload!
+  }
+
+  type Post {
+    id: ID!
+    title: String!
+    content: String!
+    createdAt: String!
+    published: Boolean!
+    user: User!
+  }
+
+  type User {
+    id: ID!
+    name: String!
+    email: String!
+    posts: [Post!]!
+  }
+
+  type Profile {
+    id: ID!
+    bio: String!
+    isMyProfile: Boolean!
+    user: User!
+  }
+
+  type UserError {
+    message: String!
+  }
+
+  type PostPayload {
+    userErrors: [UserError!]!
+    post: Post
+  }
+
+  input PostInput {
+    title: String
+    content: String
+  }
 `;
