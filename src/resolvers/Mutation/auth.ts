8a005fa0ff0b4751450613ec9d99de6ac24ccb19
Vasyl Pryimak <vasyl.pryimak.dev@gmail.com>
2026-07-16 02:52:06 +0300

    feat: add signup validation and JWT authentication

--- src/resolvers/Mutation/auth.ts
+++ src/resolvers/Mutation/auth.ts
@@ -1,25 +1,107 @@
 import { Context } from "../../index";
+import validator from "validator";
+import bcrypt from "bcryptjs";
+import JWT from "jsonwebtoken";
+import { JSON_SIGNATURE } from "../../keys";
 
 interface SignupArgs {
-  email: string;
-  password: string;
+  credentials: {
+    email: string;
+    password: string;
+  };
   name: string;
   bio: string;
+}
+
+interface SigninArgs {
+  credentials: {
+    email: string;
+    password: string;
+  };
+}
+
+interface UserPayload {
+  userErrors: {
+    message: string;
+  }[];
+  token: string | null;
 }
 
 export const authResolvers = {
   signup: async (
     _: any,
-    { name, email, password, bio }: SignupArgs,
+    { credentials, name, bio }: SignupArgs,
     { prisma }: Context,
-  ) => {
-    return prisma.user.create({
+  ): Promise<UserPayload> => {
+    const { email, password } = credentials;
+
+    const isEmail = validator.isEmail(email);
+
+    if (!isEmail) {
+      return {
+        userErrors: [
+          {
+            message: "Invalid email",
+          },
+        ],
+        token: null,
+      };
+    }
+
+    const isValidPassword = validator.isLength(password, {
+      min: 5,
+    });
+
+    if (!isValidPassword) {
+      return {
+        userErrors: [
+          {
+            message: "Invalid password",
+          },
+        ],
+        token: null,
+      };
+    }
+
+    if (!name || !bio) {
+      return {
+        userErrors: [
+          {
+            message: "Invalid name or bio",
+          },
+        ],
+        token: null,
+      };
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await prisma.user.create({
       data: {
         email,
         name,
-        password,
-        bio,
+        password: hashedPassword,
       },
     });
+
+    await prisma.profile.create({
+      data: {
+        bio,
+        userId: user.id,
+      },
+    });
+
+    return {
+      userErrors: [],
+      token: JWT.sign(
+        {
+          userId: user.id,
+        },
+        JSON_SIGNATURE,
+        {
+          expiresIn: 3600000,
+        },
+      ),
+    };
   },
 };
