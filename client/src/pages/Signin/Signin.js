a39b8280c4aaed6336cec92a8472e14be4a06853
Vasyl Pryimak <vasyl.pryimak.dev@gmail.com>
2026-07-17 18:01:48 +0300

    feat(auth): implement sign-in with GraphQL mutation

--- client/src/pages/Signin/Signin.js
+++ client/src/pages/Signin/Signin.js
@@ -1,15 +1,46 @@
 import React, { useState, useEffect } from "react";
-
+import { gql, useMutation } from "@apollo/client";
 import { Form } from "react-bootstrap";
 import Button from "@restart/ui/esm/Button";
 
+const SIGNUP = gql`
+  mutation Signup($email: String!, $password: String!) {
+    signin(credentials: { email: $email, password: $password }) {
+      userErrors {
+        message
+      }
+      token
+    }
+  }
+`;
+
 export default function Signin() {
+  const [signup, { data, loading }] = useMutation(SIGNUP);
+
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  const handleClick = () => {};
+  const handleClick = () => {
+    signup({
+      variables: {
+        email,
+        password,
+      },
+    });
+  };
 
   const [error, setError] = useState(null);
+
+  useEffect(() => {
+    if (data) {
+      if (data.signin.userErrors.length) {
+        setError(data.signin.userErrors[0].message);
+      }
+      if (data.signin.token) {
+        localStorage.setItem("token", data.signin.token);
+      }
+    }
+  }, [data]);
 
   return (
     <div>
