19b0bc511ab3575b241266a1ab9aecd7362820d6
Vasyl Pryimak <vasyl.pryimak.dev@gmail.com>
2026-07-17 17:35:39 +0300

    feat(profile): fetch and display profile data with GraphQL

--- client/src/pages/Profile/Profile.js
+++ client/src/pages/Profile/Profile.js
@@ -1,10 +1,42 @@
+import { useQuery, gql } from "@apollo/client";
 import React from "react";
 import { useParams } from "react-router";
 import AddPostModal from "../../components/AddPostModal/AddPostModal";
 import Post from "../../components/Post/Post";
 
+const GET_PROFILE = gql`
+  query GetProfile($userId: ID!) {
+    profile(userId: $userId) {
+      bio
+      isMyProfile
+      user {
+        name
+        posts {
+          id
+          title
+          content
+          createdAt
+          published
+        }
+      }
+    }
+  }
+`;
+
 export default function Profile() {
   const { id } = useParams();
+
+  const { data, error, loading } = useQuery(GET_PROFILE, {
+    variables: {
+      userId: id,
+    },
+  });
+
+  if (error) return <div>error page</div>;
+
+  if (loading) return <div>Spinner...</div>;
+
+  const { profile } = data;
 
   return (
     <div>
@@ -16,12 +48,26 @@ export default function Profile() {
         }}
       >
         <div>
-          <h1>Profile Name</h1>
-          <p>Profile Bio</p>
+          <h1>{profile.user.name}</h1>
+          <p>{profile.bio}</p>
         </div>
-        <div>{"profile" ? <AddPostModal /> : null}</div>
+        <div>{profile.isMyProfile ? <AddPostModal /> : null}</div>
       </div>
-      <div></div>
+      <div>
+        {profile.user.posts.map((post) => {
+          return (
+            <Post
+              title={post.title}
+              content={post.content}
+              date={post.createdAt}
+              user={profile.user.name}
+              published={post.published}
+              isMyProfile={profile.isMyProfile}
+              id={post.id}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 }
